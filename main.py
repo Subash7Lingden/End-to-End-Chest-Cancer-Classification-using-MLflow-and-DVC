3c5d838de8a50fa18cedb03379d34f174b180d11
Subash7Lingden <subashsubbalingden@gmail.com>
2024-04-09 09:47:02 +0545

    data ingestion added

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import os 
-from pathlib import Path
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Runnign the custom log")
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- template.py
+++ template.py
@@ -1,19 +1,19 @@
-import os 
+import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
 project_name = "cnnClassifier"
 
 list_of_files = [
-    ".github/workflow/.gitkeep",
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/config/__init__.py"
-    f"src/{project_name}/config/configuation.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
@@ -22,9 +22,13 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trails.ipynb",
+    "research/trials.ipynb",
     "templates/index.html"
+
+
 ]
+
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -33,13 +37,13 @@ for filepath in list_of_files:
 
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating direcotires: {filepath} for the file: {filename}")
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
 
     else:
-        logging.info(f"{filename} already is already exists")
+        logging.info(f"{filename} is already exists")
